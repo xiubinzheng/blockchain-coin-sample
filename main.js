b6c33981537f542e7703bc297ab6174fa9ab855e
xiubinzheng <xiubin.zheng7@gmail.com>
2017-12-08 23:00:37 -0500

    we now have a working mini blockchain.

--- main.js
+++ main.js
@@ -7,11 +7,10 @@ class Block{
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
-
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
@@ -22,5 +21,28 @@ class BlockChain{
         this.chain = [this.createGenesisBlock()];
     }
 
+    createGenesisBlock(){
+        return new Block(0,"01/01/2017","Genesis block","0");
+    }
+
+    // return the latest block in the chain
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock)
+    {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        // recalculate the hash of the block
+        newBlock.hash = newBlock.calculateHash();
+        // need to push onto the block
+        this.chain.push(newBlock);
+    }
 
 }
+
+let mycoin = new BlockChain();
+mycoin.addBlock(new Block(1,"10/06/2017",{amount: 12}));
+mycoin.addBlock(new Block(2,"10/16/2017",{amount: 22}));
+
+console.log(JSON.stringify(mycoin,null,4));
